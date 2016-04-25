a1dca72840ec33bde98ebbdb7063893b3fa41501
Michael Maher <micmaher@users.noreply.github.com>
2016-04-25 20:59:25 +0100

    Update SuperPingLocalSubnet.ps1

--- SuperPingLocalSubnet.ps1
+++ SuperPingLocalSubnet.ps1
@@ -1,14 +1,22 @@
-#Requires -Version 3.0
 #Requires -Modules Test-ConnectionAsync
-#https://github.com/proxb/AsyncFunctions/blob/master/Test-ConnectionAsync.ps1
+#Requires -Version 3.0
+
+# To get Test-ConnectionAsync in PowerShell 5.0 use Package Management 
+# Install-Module -Name TestConnectionAsync
+
+# If earlier version of PowerShell, download from Boe Prox's GitHub repo
+# https://github.com/proxb/AsyncFunctions/blob/master/Test-ConnectionAsync.ps1
+       
+[cmdletbinding()]
+Param()
 
 $myIP = Get-NetIPConfiguration | Select IPv4Address
-$octect = $myIP.IPv4Address.IPv4Address -split "\." # backslah means newline in RegEx
+$octect = $myIP.IPv4Address.IPv4Address -split "\." # backslash means newline in RegEx
 
-# Get the IP range for the subnet you are on
 $subnet = for ($i = 1; $i -lt 255; $i += 1){
         [PSCustomObject]@{
             testIP = "$($octect.Item(0)).$($octect.Item(1)).$($octect.Item(2)).$($i)"
             }
 }               
+
 Test-ConnectionAsync -Computer $subnet.testip | select computername, result | where result -eq Success
